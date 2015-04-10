c1f4687b1068f9b9d8093a55128aa1ecf28ce439
adam-gheist-may <adam.may@asu.edu>
2015-04-10 11:12:30 -0700

    Bugs 001 & 002 fixed

--- FourRowSolitaire/src/FourRowSolitaire/Card.java
+++ FourRowSolitaire/src/FourRowSolitaire/Card.java
@@ -48,7 +48,7 @@ public class Card extends JComponent
     public static final int THREE = 3;
     public static final int FOUR = 4;
     //BUG 0002
-    public static final int FIVE = 6;
+    public static final int FIVE = 5;
     public static final int SIX = 6;
     public static final int SEVEN = 7;
     public static final int EIGHT = 8;
